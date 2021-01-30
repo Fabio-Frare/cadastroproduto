86d4a511022ea522a179c147d085346820d1f96f
Fabio-Frare <44546034+Fabio-Frare@users.noreply.github.com>
2021-01-30 11:39:23 -0300

    Commit inicial

---
+++ cadastroproduto/src/main/java/br/com/cadastroproduto/cadastroproduto/CadastroprodutoApplication.java
@@ -0,0 +1,13 @@
+package br.com.cadastroproduto.cadastroproduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroprodutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroprodutoApplication.class, args);
+	}
+
+}
